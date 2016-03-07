0159599840512bb7c0a831dcd819d3d5d62c7c65
stor0095 <geemakunstorey@geemakuns-mbp.ottawa.ad.algonquincollege.com>
2016-03-07 11:20:46 -0500

    Finished.
    Got rid of click event in exchange for Hammer JS library. Fixed css
    transitions.

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@ function init(ev){
   //add listeners to buttons
   var pl = document.querySelectorAll(".page-link");
   [].forEach.call(pl, function(obj, index){
-    //console.log(index, obj);
-    //use touchend if this is for mobile only
-    obj.addEventListener("click", navigate);
+     var navTap = new Hammer(obj);
+      navTap.on('tap', navigate);
   });
   //add listeners to pages
     
@@ -23,24 +22,6 @@ function init(ev){
     //transitionend or animationend listeners
     obj.addEventListener("animationend", pageAnimated);
   });
- // NAV BAR - HAMMER JS EVENTS
-    var homeNav = document.querySelector(".homeButton");
-    var castNav = document.querySelector(".castButton");
-    var reviewNav = document.querySelector(".reviewButton");
-    
-    var homeButton = new Hammer(homeNav);
-    var castButton = new Hammer(castNav);
-    var reviewButton = new Hammer(reviewNav);
-    homeButton.on('tap', function(ev){
-        console.log("Tap worked");
-    });
-    castButton.on('tap', function(ev){
-        console.log("Tap worked");
-    });
-    reviewButton.on('tap', function(ev){
-        console.log("Tap worked");
-    });
-    
     // HAMMER JS FOR ROTTEN TOMATOES LINK
     var rottenLink = document.querySelector(".webLink");
     var rottenFunc = new Hammer(rottenLink);
@@ -48,7 +29,6 @@ function init(ev){
         // Alert the user that they are leaving the site
     var confirmMessage = "You are being taken to Rotten Tomatoes";
     var go = "http://www.rottentomatoes.com/m/inception/";
-    
     if (alert(confirmMessage)){
         window.location = go;
     } 
@@ -62,13 +42,9 @@ function navigate(ev){
   //history.pushState();
   var pages = document.querySelectorAll('[data-role="page"]');
   for(var p=0; p<pages.length; p++){
-    //console.log(pages[p].id, page);
     if(pages[p].id === id){
       pages[p].classList.remove("hidden");
-      if(pages[p].className !== "active-page"){
         pages[p].className = "active-page";
-      }
-      //console.log("active ", page)
     }else{
       if(pages[p].className !== "inactive-page"){
         pages[p].className = "inactive-page";
@@ -77,14 +53,8 @@ function navigate(ev){
   }
 }
 function pageAnimated(ev){
-//  console.log("Transition finished for " + ev.target.id);
-//  console.dir(ev);
-//    console.log(ev);
 var page = ev.target;
   if( page.className == "active-page" ){
     console.log(ev.target.id, " has just appeared");
-  }else{
-//    console.log(ev.target.id, " has just disappeared");
-    ev.target.classList.add("hidden");
   }
 }
